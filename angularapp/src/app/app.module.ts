153c5aa140d14cd9d9756b2d22c973c230658c4d
Praveen-eee <praveenvellingiri02@gmail.com>
2023-05-14 14:55:47 +0000

    v2

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
