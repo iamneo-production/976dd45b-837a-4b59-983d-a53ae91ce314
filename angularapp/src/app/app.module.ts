9335cbf6797570fd7096d1270f872f3ebd82a3b3
Praveen-eee <praveenvellingiri02@gmail.com>
2023-06-02 06:07:55 +0000

    karma file structure(ang)

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -16,10 +16,15 @@ import { MybookingComponent } from './components/customerside/mybooking/mybookin
 import { NavbarUserComponent } from './components/customerside/navbar-user/navbar-user.component';
 import { UserHomepageComponent } from './components/customerside/user-homepage/user-homepage.component';
 import { BookingComponent } from './components/adminside/booking/booking.component';
+import { AddonsComponent } from './components/adminside/addons/addons.component';
+import { DashboardComponent } from './components/adminside/dashboard/dashboard.component';
+import { FoodmenuComponent } from './components/adminside/foodmenu/foodmenu.component';
+import { NavbarAdminComponent } from './components/adminside/navbar-admin/navbar-admin.component';
+import { ThemesComponent } from './components/adminside/themes/themes.component';
+import { EditEventComponent } from './components/customerside/edit-event/edit-event.component';
+import { UserreviewComponent } from './components/customerside/userreview/userreview.component';
+import { AdminreviewComponent } from './components/adminside/adminreview/adminreview.component';
 
-
-
-// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -36,13 +41,20 @@ import { BookingComponent } from './components/adminside/booking/booking.compone
     MybookingComponent,
     NavbarUserComponent,
     UserHomepageComponent,
-    BookingComponent
+    BookingComponent,
+    AddonsComponent,
+    DashboardComponent,
+    FoodmenuComponent,
+    NavbarAdminComponent,
+    ThemesComponent,
+    EditEventComponent,
+    UserreviewComponent,
+    AdminreviewComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angularapp/src/app/components/adminside/foodmenu/foodmenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foodmenu',
+  templateUrl: './foodmenu.component.html',
+  styleUrls: ['./foodmenu.component.css']
+})
+export class FoodmenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
