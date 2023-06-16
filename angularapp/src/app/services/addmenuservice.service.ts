14cc9c92bc055edd463967f0deb435ab49a0bdb6
yogi-karnati <yogendrakarnati777@gmail.com>
2023-06-16 12:48:25 +0000

    Yogendra - Solved ts lint errors(all addon components,addmenu and addon services)

--- angularapp/src/app/services/addmenuservice.service.ts
+++ angularapp/src/app/services/addmenuservice.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Addmenu } from '../class/addmenu';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
@@ -14,25 +14,25 @@ export class AddmenuserviceService {
   private baseUrl3 = "https://8080-bbccbbbafbbadefeafcdfecbcbddcca.project.examly.io/admin/getMenu";
   private baseUrl4 = "https://8080-bbccbbbafbbadefeafcdfecbcbddcca.project.examly.io/admin/deleteMenu";
 
-  constructor(private httpclient : HttpClient) { }
+  constructor(private httpclient: HttpClient) { }
 
   getMenu(): Observable<Addmenu[]>{
     return this.httpclient.get<Addmenu[]>(`${this.baseUrl3}`);
   }
 
-  addMenu(addmenu : Addmenu): Observable<Object>{
-    return this.httpclient.post(`${this.baseUrl}`,addmenu);
+  addMenu(addmenu: Addmenu): Observable<object>{
+    return this.httpclient.post(`${this.baseUrl}`, addmenu);
   }
 
-  getAddmenuById(foodMenuID: number):Observable<Addmenu>{
+  getAddmenuById(foodMenuID: number): Observable<Addmenu>{
     return this.httpclient.get<Addmenu>(`${this.baseUrl3}/${foodMenuID}`);
   }
 
-  editMenu(foodMenuID: number,addmenu : Addmenu):Observable<Object>{
-    return this.httpclient.put(`${this.baseUrl2}/${foodMenuID}`,addmenu);
+  editMenu(foodMenuID: number, addmenu: Addmenu): Observable<object>{
+    return this.httpclient.put(`${this.baseUrl2}/${foodMenuID}`, addmenu);
   }
 
-  deleteMenu(foodMenuID: number): Observable<Object>{
+  deleteMenu(foodMenuID: number): Observable<object>{
     return this.httpclient.delete(`${this.baseUrl4}/${foodMenuID}`);
   }
 
