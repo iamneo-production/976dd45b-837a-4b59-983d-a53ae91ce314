44ceb24df71426017cd382386d8d913c901f95dc
Navyasree-Siram <uec19312@rmd.ac.in>
2023-06-22 11:44:13 +0000

    Navya contribution

--- springapp/src/main/java/com/examly/springapp/Controller/EventBookingController.java
+++ springapp/src/main/java/com/examly/springapp/Controller/EventBookingController.java
@@ -14,7 +14,6 @@ public class EventBookingController
 
   @Autowired
   private EventService eservice;
-  
 
   @PostMapping("/user/bookTheme")
   public EventModel bookEvent(@RequestBody EventModel add)
@@ -26,58 +25,15 @@ public class EventBookingController
   @GetMapping("/user/getBookedTheme")
   public List<EventModel> viewEvent()
   {
-  List<EventModel> events = eservice.viewAllEvent();
-
+    List<EventModel> events = eservice.viewAllEvent();
     return events;
   }
-  @GetMapping("/user/getBookedTheme/{themeId}")
-  public EventModel  viewEventbyId(@PathVariable int themeId)
-  {
-    EventModel add = eservice.viewEvent(themeId);
-    return add;
-  }
-  @PutMapping("/user/editTheme/{themeId}")
-  public String editEvent(@PathVariable int themeId,@RequestBody EventModel e)
-  {
-
-    EventModel event = eservice.findById(themeId);
-    if(event==null)
-    {
-      return   "\"No data Exist\" ";
-    }
-    event.setEventName(e.getEventName());
-    event.setEventAddress(e.getEventAddress());
-    event.setApplicantName(e.getApplicantName());
-    event.setApplicantEmail(e.getApplicantEmail());
-    event.setApplicantMobile(e.getApplicantMobile());
-    event.setApplicantAddress(e.getApplicantAddress());
-    event.setEventDate(e.getEventDate());
-    event.setEventFromTime(e.getEventFromTime());
-    event.setEventToTime(e.getEventToTime());
-    event.setEventTime(e.getEventTime());
-    event.setEventCost(e.getEventCost());
-    event.setAddonId(e.getAddonId());
-    event.setEventMenuId(e.getEventMenuId());
-    eservice.editEvent(event);
-   
-    return   "\"Updated Successfully\" ";
-
-  }
-
+  
   @DeleteMapping("/user/deleteTheme/{themeId}")
   public String deleteEvent(@PathVariable int themeId)
   {
-  
     eservice.deleteEvent(themeId);
     return "\"Event deleted\"";
-
-
   }
-  //unique
-  @GetMapping("/mybooking/{userId}")
-  public List<EventModel> getbookingById(@PathVariable long userId) {
-    List<EventModel> booking = eservice.getbookingById(userId);
-   return booking;
- }
 
 }
