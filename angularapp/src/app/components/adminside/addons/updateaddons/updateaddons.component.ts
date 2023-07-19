abca7a567cab38afc9ce4e6a78d942b3e11d0052
yogi-karnati <yogendrakarnati777@gmail.com>
2023-07-19 11:31:10 +0000

    Yogendra - addon Validation and button

--- angularapp/src/app/components/adminside/addons/updateaddons/updateaddons.component.ts
+++ angularapp/src/app/components/adminside/addons/updateaddons/updateaddons.component.ts
@@ -2,41 +2,57 @@ import { Component, OnInit } from '@angular/core';
 import { Addon } from '../../../../class/addon';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AddonserviceService } from '../../../../services/addonservice.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-updateaddons',
   templateUrl: './updateaddons.component.html',
   styleUrls: ['./updateaddons.component.css']
 })
-export class UpdateaddonsComponent implements OnInit{
+export class UpdateaddonsComponent implements OnInit {
   addOnid = 0;
   newaddon: Addon = new Addon();
+  addonForm: FormGroup;
+
   addonid = 'addOnid';
 
-  constructor(private router: Router, private addservice: AddonserviceService, private route: ActivatedRoute){
+  constructor(private router: Router, private addservice: AddonserviceService, private route: ActivatedRoute, private fb: FormBuilder, private toastr: ToastrService) {}
 
-  }
+  ngOnInit(): void {
+    this.addonForm = this.fb.group({
+      addOnName: ['', [Validators.required, Validators.pattern("[a-zA-Z][a-zA-Z ]+")]],
+      addAddonPrice: ['', [Validators.required, Validators.pattern("^[1-9][0-9]*")]],
+      addonDescription: ['']
+    })
 
-
-  ngOnInit(): void{
     this.addOnid = this.route.snapshot.params[this.addonid];
-    this.addservice.getAddonId(this.addOnid).subscribe(data => {
-    this.newaddon = data;
-    });
+    this.addservice.getAddonId(this.addOnid).subscribe((data) => {
+      console.log(data);
+      this.newaddon = data;
+    },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 
-  gotoaddon(): void{
+  gotoaddon(): void {
     this.router.navigate(['admin/addon']);
   }
 
-  onSubmit(): void{
-    this.addservice.editAddon(this.addOnid, this.newaddon).subscribe(data => {
-    this.gotoaddon();
-    } );
+  onSubmit(): void {
+    this.addservice.editAddon(this.addOnid, this.newaddon).subscribe((data) => {
+      console.log(data);
+      this.gotoaddon();
+    },
+      (error) => {
+        console.log(error.error.message);
+        this.toastr.warning(error.error.message, 'Addon Status');
+      });
   }
-  
-  back(): any{
-  
+
+  back(): any {
     this.router.navigate(['admin/addon']);
   }
 }
