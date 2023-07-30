1fb51ac964866cb3e933ef0c1194124a667bea49
Jaisuriya0405 <85072745+Jaisuriya0405@users.noreply.github.com>
2023-07-30 17:21:54 +0530

    Update AuthController.java

--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -9,7 +9,7 @@ import com.examly.springapp.Service.UserService;
 
 
 @RestController
-@CrossOrigin(origins = "https://8081-bbccbbbafbbadefeafcdfecbcbddcca.project.examly.io")
+@CrossOrigin(origins = "https://8081-beddbfbbfbabaccccfeafbbadefeafcdfecbcbddcca.project.examly.io")
 @RequestMapping
 public class AuthController {
     @Autowired
